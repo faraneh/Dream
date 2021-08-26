0d1d89310ff03cdff72dfc058d928e1b9101964d
Janna <78837635+faraneh@users.noreply.github.com>
2021-08-25 20:54:40 -0400

    projectsPage(80%)
    links remain

---
+++ src/components/projectComponents/travelBlog/travelBlog.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import'./travelBlog.css';
+
+class TravelBlog extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className="TravelBlog">Travel Blog</div>
+         );
+    }
+}
+ 
+export default TravelBlog;
